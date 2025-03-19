674df1424a609ac9a76aff5117204075ccdc6f15
ishanPamodyaa <inishan9999@gmail.com>
2025-03-19 23:49:45 +0530

    some component

--- motorcycle-parts-ecommerce/frontend ui/src/app/app.component.ts
+++ motorcycle-parts-ecommerce/frontend ui/src/app/app.component.ts
@@ -3,6 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './component/header/header.component';
 import { FooterComponent } from './component/footer/footer.component';
 import { AdminHeaderComponent } from './component/admin-header/admin-header.component';
+import {LoginComponent} from './component/login/login.component';
+import {SaleComponent} from './component/page/sale/sale.component';
+import {SignUpComponent} from './component/sign-up/sign-up.component';
+import {ProductComponent} from './component/product/product.component';
+import {SingleProductComponent} from './component/single-product/single-product.component';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +16,11 @@ import { AdminHeaderComponent } from './component/admin-header/admin-header.comp
     HeaderComponent,
     FooterComponent,
     AdminHeaderComponent,
+    LoginComponent,
+    SaleComponent,
+    SignUpComponent,
+    ProductComponent,
+    SingleProductComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
