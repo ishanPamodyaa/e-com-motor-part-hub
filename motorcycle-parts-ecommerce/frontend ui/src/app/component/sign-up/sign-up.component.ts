1f459ae5189b1a10ec28a860a63c407cb3e92e7c
ishanPamodyaa <inishan9999@gmail.com>
2025-04-01 00:39:02 +0530

    Authentication imlement

--- motorcycle-parts-ecommerce/frontend ui/src/app/component/sign-up/sign-up.component.ts
+++ motorcycle-parts-ecommerce/frontend ui/src/app/component/sign-up/sign-up.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import {FormGroup, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent {
+
+  signupForm:FormGroup;
 
 }
